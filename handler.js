4706b66f79c2d7c7dafddd4220cef194f1bb67cc
Ina Burström <inaburstrom@hotmail.com>
2024-11-27 18:17:05 +0100

    Implemented middy and updated with all functions.

--- handler.js
+++ handler.js
@@ -1,6 +1,31 @@
+const middy = require('@middy/core');
+const { authenticate } = require('./middleware/auth');
 const { getNotes, addNote, updateNote, deleteNote } = require('./functions/notes');
 
-module.exports.getNotes = getNotes;
-module.exports.addNote = addNote;
-module.exports.updateNote = updateNote;
-module.exports.deleteNote = deleteNote;
+const getNotesHandler = async (event) => {
+  const userId = event.user.email; 
+  return await getNotes(userId); 
+};
+
+const addNoteHandler = async (event) => {
+  const noteData = JSON.parse(event.body);
+  noteData.userId = event.user.email; 
+  return await addNote(noteData); 
+};
+
+const updateNoteHandler = async (event) => {
+  const noteData = JSON.parse(event.body); 
+  noteData.userId = event.user.email;  
+  return await updateNote(noteData); 
+};
+
+const deleteNoteHandler = async (event) => {
+  const { id } = event.pathParameters; 
+  const userId = event.user.email; 
+  return await deleteNote(id, userId);  
+};
+
+module.exports.getNotes = middy(getNotesHandler).use(authenticate());
+module.exports.addNote = middy(addNoteHandler).use(authenticate());
+module.exports.updateNote = middy(updateNoteHandler).use(authenticate());
+module.exports.deleteNote = middy(deleteNoteHandler).use(authenticate());
